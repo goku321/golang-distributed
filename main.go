d6c89daf0b6018f3404de92755da818b1476cadf
Deepak Sah <sah.sslpu@gmail.com>
2019-09-01 12:52:22 +0530

    Update main.go
    Fixed indentation

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type data struct {
 }
 
 func main() {
-	clusterIp := flag.String("clusterip", "127.0.0.1:8001", "ip address of slave node")
+  clusterIp := flag.String("clusterip", "127.0.0.1:8001", "ip address of slave node")
   port := flag.String("port", "8001", "port to use")
   flag.Parse()
 }
